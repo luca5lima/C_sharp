d5c347f861582c14e4698a3c88b2a30802710997
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-05-18 16:47:05 -0300

    Project3

---
+++ CsharpProjects/Project3/Program.cs
@@ -0,0 +1,55 @@
+﻿// /*
+// string[] fraudulentOrderIDs = new string[3];
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+// // fraudulentOrderIDs[3] = "D000";
+// */
+// string[] fraudulentOrderIDs = {"A123", "B456", "C789"};
+
+// Console.WriteLine($"\nFirst: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+// // fraudulentOrderIDs[0] = "F000";
+
+// Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}\n");
+
+// Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.\n");
+
+//##############################################################
+/*
+string[] names = {"Rowena", "Robin", "Bao", "Matheus"};
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+*/
+//##############################################################
+/*
+int[] inventory = {200, 450, 700, 175, 250};
+int soma = 0;
+int i = 0;
+foreach (int product in inventory)
+{
+    soma = soma + product;
+    i = i + 1;
+    Console.WriteLine($"Bin: {i} = {product} items (Running total: {soma})");
+}
+Console.WriteLine($"\nWe have {soma} items in inventory.");
+*/
+//##############################################################
+
+string[] orderIDs = {
+    "B123",    "C234",     "A345",    
+    "C15" ,    "B177",    "G3003",
+    "C235",    "B179"
+};
+
+foreach (string IDs in orderIDs)
+{
+    if (IDs.StartsWith("B"))
+    {
+        Console.WriteLine($"IDs com a letra B: {IDs}");
+    }
+}
--- CsharpProjects/Project3/obj/Debug/net8.0/Project3.AssemblyInfo.cs
+++ CsharpProjects/Project3/obj/Debug/net8.0/Project3.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Project3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10d0131c8635c06b23480081a23dd09937ff0e7e")]
-[assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
+[assembly: System.Reflection.AssemblyProductAttribute("Project3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Project3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
